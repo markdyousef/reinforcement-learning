4de34358505fc0647c4fc6f3533a188336675af2
markdyousef <markdyousef@gmail.com>
2017-11-01 12:54:33 +0100

    implemented simple sarsa forward on windy gridworld

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 from matplotlib.cm import coolwarm
 import numpy as np
+from collections import namedtuple
 from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
+
+EpisodeStats = namedtuple("Stats", ["episode_lengths", "episode_rewards"])
 
 # plots value function for blackjack env
 def plot_value_function(V, title="Value Function"):
@@ -33,3 +37,42 @@ def plot_value_function(V, title="Value Function"):
     
     plot_surface(X, Y, Z_noace, f'{title} (No Usable Ace)')
     plot_surface(X, Y, Z_ace, f'{title} (Usable Ace)')
+
+def plot_episode_stats(stats, smoothing_window=10, noshow=False):
+    # plot episode length over time
+    fig1 = plt.figure(figsize=(10, 5))
+    plt.plot(stats.episode_lengths)
+    plt.xlabel("Episode")
+    plt.ylabel("Episode Length")
+    plt.title("Episode Length over Time")
+    if noshow:
+        plt.close(fig1)
+    else:
+        plt.show(fig1)
+    
+    # plot episode reward over time
+    fig2 = plt.figure(figsize=(10, 5))
+    rewards_smooth = pd.Series(stats.episode_rewards).rolling(smoothing_window,
+                                                              min_periods=smoothing_window).mean()
+    plt.plot(rewards_smooth)
+    plt.xlabel("Episode")
+    plt.ylabel("Episode Reward (Smoothed)")
+    plt.title("Episode Reward over Time (Smoothed over window size {})".format(smoothing_window))
+    if noshow:
+        plt.close(fig2)
+    else:
+        plt.show(fig2)
+    
+    # plot time steps and episode number
+    fig3 = plt.figure(figsize=(10, 5))
+    plt.plot(np.cumsum(stats.episode_lengths), np.arange(len(stats.episode_lengths)))
+    plt.xlabel("Time Steps")
+    plt.ylabel("Episodes")
+    plt.title("Episode per Time Step")
+    if noshow:
+        plt.close(fig3)
+    else:
+        plt.show(fig3)
+    
+    return fig1, fig2, fig3
+    

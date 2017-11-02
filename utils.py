8d67b689f5aba9107a8ce6fefe17cea2f72cd58d
markdyousef <markdyousef@gmail.com>
2017-11-02 16:41:28 +0100

    semi-gradient sarsa on mountain-car env

--- utils.py
+++ utils.py
@@ -75,4 +75,20 @@ def plot_episode_stats(stats, smoothing_window=10, noshow=False):
         plt.show(fig3)
     
     return fig1, fig2, fig3
-    
+
+def plot_cost_to_go_mountain_car(env, estimator, num_tiles=20):
+    x = np.linspace(env.observation_space.low[0], env.observation_space.high[0], num=num_tiles)
+    y = np.linspace(env.observation_space.low[1], env.observation_space.high[1], num=num_tiles)
+    X, Y = np.meshgrid(x, y)
+    Z = np.apply_along_axis(lambda _: -np.max(estimator.predict(_)), 2, np.dstack([X, Y]))
+
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(111, projection='3d')
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                           cmap=coolwarm, vmin=-1.0, vmax=1.0)
+    ax.set_xlabel('Position')
+    ax.set_ylabel('Velocity')
+    ax.set_zlabel('Value')
+    ax.set_title("Mountain \"Cost To Go\" Function")
+    fig.colorbar(surf)
+    plt.show()
